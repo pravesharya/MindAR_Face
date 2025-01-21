e75519b05bf8c91f36803a8df2238513042a8182
pravesharya <pravesharya925@gmail.com>
2025-01-22 02:00:16 +0530

    myMask00

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { TextureLoader } from "three";
-import { GLTFLoader } from "GLTFLoader";
+// import { GLTFLoader } from "GLTFLoader";
 import { MindARThree } from "mindAR";
 
 // const canvas = document.querySelector("#canvas");
@@ -8,11 +8,11 @@ const btnF = document.querySelector("#F");
 const btnC = document.querySelector("#C");
 const btnS = document.querySelector("#S");
 
-const loaderGltf = new GLTFLoader();
-const modelPath = "./assets/eye01.glb";
+// const loaderGltf = new GLTFLoader();
+// const modelPath = "./assets/eye01.glb";
 
 const textureLoader = new THREE.TextureLoader();
-const maskPath = "./assets/clownMask02.png";
+const maskPath = "./assets/myMask00.png";
 // const occluderPath = "./assets/head_occluder_01.glb";
 
 // const loaderAudio = new THREE.AudioLoader();
@@ -35,17 +35,17 @@ async function startMindAR() {
   // anchorNose.group.add(sphere);
 
   // Wait for model to load
-  const gltf = await new Promise((resolve) => {
-    loaderGltf.load(modelPath, (gltf) => {
-      resolve(gltf);
-    });
-  });
-  model = gltf.scene;
-  const anchorEye = mindAR.addAnchor(168);
-  anchorEye.group.add(model);
-  let size = 0.125;
-  model.scale.set(size, size, size);
-  model.position.set(0,-0.1,-0.05);
+  // const gltf = await new Promise((resolve) => {
+  //   loaderGltf.load(modelPath, (gltf) => {
+  //     resolve(gltf);
+  //   });
+  // });
+  // model = gltf.scene;
+  // const anchorEye = mindAR.addAnchor(168);
+  // anchorEye.group.add(model);
+  // let size = 0.125;
+  // model.scale.set(size, size, size);
+  // model.position.set(0,-0.1,-0.05);
   
   // const gltfOccluder = await new Promise((resolve) => {
   //   loaderGltf.load(occluderPath, (gltf) => {
