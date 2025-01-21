d3066cdda1df826296e079724c6a6d1666e944a3
pravesharya <pravesharya925@gmail.com>
2025-01-21 16:28:54 +0530

    eyes

---
+++ script.js
@@ -0,0 +1,92 @@
+import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader";
+import { MindARThree } from "mindAR";
+
+// const canvas = document.querySelector("#canvas");
+const btnF = document.querySelector("#F");
+const btnC = document.querySelector("#C");
+const btnS = document.querySelector("#S");
+
+const loaderGltf = new GLTFLoader();
+const modelPath = "./assets/eye01.glb";
+
+// const loaderAudio = new THREE.AudioLoader();
+// const audioPath = "./assets/meow.mp3";
+
+let model;
+// , animations, mixer, audio, listener;
+async function startMindAR() {
+  const mindAR = new MindARThree({
+    container: document.body,
+  });
+  const { renderer, camera, scene } = mindAR;
+  
+  const light = new THREE.HemisphereLight(0xffffff, 10);
+  scene.add(light);
+  
+  const geometry = new THREE.SphereGeometry(0.1, 32, 32);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const sphere = new THREE.Mesh(geometry, material);
+  const anchorNose = mindAR.addAnchor(1);
+  anchorNose.group.add(sphere);
+
+  // Wait for model to load
+  const gltf = await new Promise((resolve) => {
+    loaderGltf.load(modelPath, (gltf) => {
+      resolve(gltf);
+    });
+  });
+  model = gltf.scene;
+  console.log(model);
+  const anchorEye = mindAR.addAnchor(168);
+  anchorEye.group.add(model);
+  let size = 0.125;
+  // let size = 0.15;
+  model.scale.set(size, size, size);
+  // animations = gltf.animations;
+  
+
+  // mixer = new THREE.AnimationMixer(model);
+  // const action = mixer.clipAction(animations[0]);
+  // action.play();
+
+  // const audioClip = await new Promise((resolve) => {
+  //   loaderAudio.load(audioPath, (buffer) => {
+  //     resolve(buffer);
+  //   });
+  // });
+
+  // listener = new THREE.AudioListener();
+  // camera.add(listener);
+  // audio = new THREE.PositionalAudio(listener);
+  // audio.setBuffer(audioClip);
+  // audio.setRefDistance(100);
+  // // audio.setLoop(true);
+  
+  // anchor.onTargetFound = () => {
+  //   setInterval(() => {
+  //     audio.isPlaying ? audio.pause() : audio.play();
+  //   }, 5000);
+  //   // audio.play();
+  // } 
+  // anchor.onTargetLost = () => {
+  //   audio.pause();
+  // } 
+
+  await mindAR.start();
+
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene, camera);
+  });
+}
+startMindAR();
+
+btnF.addEventListener("click", () => {
+  console.log("Filters");
+});
+btnC.addEventListener("click", () => {
+  console.log("Capture");
+});
+btnS.addEventListener("click", () => {
+  console.log("Switch");
+});
