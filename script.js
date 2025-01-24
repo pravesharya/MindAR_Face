2b86b43e5827d3fe2b1a46451bf5af47ae24302d
pravesharya <pravesharya925@gmail.com>
2025-01-24 14:58:52 +0530

    download SS

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ import { TextureLoader } from "three";
 // import { GLTFLoader } from "GLTFLoader";
 import { MindARThree } from "mindAR";
 
+let width = window.innerWidth;
+let height = window.innerHeight;
+
 // const canvas = document.querySelector("#canvas");
-// const btnF = document.querySelector("#F");
-// const btnC = document.querySelector("#C");
+const btnF = document.querySelector("#F");
+const btnC = document.querySelector("#C");
 const btnS = document.querySelector("#S");
 
 // const loaderGltf = new GLTFLoader();
@@ -24,6 +27,7 @@ async function startMindAR() {
     container: document.body,
   });
   const { renderer, camera, scene } = mindAR;
+  renderer.setSize(width, height);
 
   const light = new THREE.HemisphereLight(0xffffff, 10);
   scene.add(light);
@@ -83,17 +87,52 @@ async function startMindAR() {
 }
 startMindAR();
 
-// btnF.addEventListener("click", () => {
-//   console.log("Filters");
-// });
-// btnC.addEventListener("click", () => {
-//   console.log("Capture");
-// });
-
-btnS.addEventListener("click", () => {
-  console.log("Switch");
+btnF.addEventListener("click", () => {
+  console.log("Filters");
   count === 0 ? (count = 1) : (count = 0);
   maskPath = `./assets/myMask0${count}.png`;
   maskTexture = textureLoader.load(maskPath);
   faceMesh.material.map = maskTexture;
 });
+btnC.addEventListener("click", () => {
+  console.log("Capture");
+  capture();
+});
+
+btnS.addEventListener("click", () => {
+  console.log("Switch Camera");
+  mindAR.switchCamera();
+});
+
+function capture () {
+  const {renderer, video, scene, camera} = mindAR;
+  
+  const canvasA = renderer.domElement;
+  const canvasB = document.createElement('canvas');
+  const context = canvasB.getContext('2d');
+  
+  console.log(canvasA,video);
+  canvasB.width = canvasA.width;
+  canvasB.height = canvasA.height;
+
+  let aX,aY,aW,aH,bX,bY,bW,bH;
+  aX = aY = aW = aH = bX = bY = bW = bH = 0 ;
+
+  aX = (video.clientWidth - canvasA.clientWidth) / 2 * (video.videoWidth / video.clientWidth);
+  aY = (video.clientHeight - canvasA.clientHeight) / 2 * (video.videoHeight / video.clientHeight);
+  aW = video.videoWidth - aX * 2;
+  aH = video.videoHeight - aY * 2;
+
+  context.drawImage(video, aX,aY,aW,aH, 0, 0, canvasB.width, canvasB.height);
+  renderer.preserveDrawingBuffer = true;
+  renderer.render(scene, camera);
+  context.drawImage(canvasA, 0, 0, canvasA.width, canvasA.height);
+  renderer.preserveDrawingBuffer = false;
+
+  const link = document.createElement('a');
+  link.href = canvasB.toDataURL('image/png');
+  link.download = 'capture.png';
+  link.click();
+}
+
+
